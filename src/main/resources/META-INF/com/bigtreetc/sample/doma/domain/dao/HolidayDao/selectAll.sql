730c8df8af5ab5aa2bb01bd754a89ff192766dcb
miyabayt <4364793+miyabayt@users.noreply.github.com>
2023-03-28 03:20:54 +0900

    feat: 検索条件に日付を指定できるようにする

--- src/main/resources/META-INF/com/bigtreetc/sample/doma/domain/dao/HolidayDao/selectAll.sql
+++ src/main/resources/META-INF/com/bigtreetc/sample/doma/domain/dao/HolidayDao/selectAll.sql
@@ -1,10 +1,16 @@
 SELECT
-  /*%expand*/*
+    /*%expand*/*
 FROM
-  holidays
+    holidays
 WHERE
 /*%if criteria.id != null */
-  AND holiday_id = /* criteria.id */1
+    AND holiday_id = /* criteria.id */1
+/*%end*/
+/*%if criteria.holidayName != null */
+    AND holiday_name LIKE /* @infix(criteria.holidayName) */'元旦'
+/*%end*/
+/*%if criteria.holidayDate != null */
+    AND holiday_date = /* criteria.holidayDate */'2000-01-01'
 /*%end*/
 ORDER BY
-  holiday_id ASC
+    holiday_id ASC
